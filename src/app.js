0dcdcb3f28482981a57b2944dfcaf876e06fb270
Jorge <jorgezg0103@gmail.com>
2026-02-16 17:56:22 +0100

    FIX auth route

--- src/app.js
+++ src/app.js
@@ -1,12 +1,12 @@
 import express from 'express';
 import cors from 'cors';
-import { router } from './routes/auth.js'
+import { auth } from './routes/auth.js'
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use('api/auth', router)
+app.use('/api/auth', auth);
 
 app.get('/', (req, res) => {
 	res.send('API working');
